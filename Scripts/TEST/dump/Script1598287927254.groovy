7392aa61fdea67b15282ed7566028d1cf952589d
mosaj <51008278+m2utester3@users.noreply.github.com>
2020-08-25 00:53:19 +0800

    commit : Testcases Folder

---
+++ Scripts/TEST/dump/Script1598287927254.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('/Users/mosaj/katalon/APK/app-uat-universal-release.apk', true)
+
+Mobile.getText(findTestObject('dump/android.widget.TextView0 - Hello'), 0)
+
+Mobile.tap(findTestObject('dump/android.widget.TextView0 - Transfer'), 0)
+
+Mobile.setText(findTestObject('dump/android.widget.EditText0'), 'pass1234', 0)
+
+Mobile.hideKeyboard()
+
+Mobile.hideKeyboard()
+
+Mobile.closeApplication()
+
